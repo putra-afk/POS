631edbbc789a4e81f17d4c8cd060ba143d0aed96
Bayu Putra L <putralaksmanabayu@gmail.com>
2025-03-04 13:31:40 +0700

    Praktikum-2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,18 +16,35 @@ class UserController extends Controller
 
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
 
         // Menyimpan data ke dalam tabel m_user
-        UserModel::create($data);
+        // UserModel::create($data);
 
+        //$user = UserModel::all();
         // Ambil semua data dari tabel m_user
-        $user = UserModel::all();
+        //$user = UserModel::find(1);
+
+        // Ambil data dari tabel m_user berdasarkan kolom level_id
+        //$user = UserModel::where('level_id', 1)->first();
+
+        // Ambil data dari tabel m_user berdasarkan kolom level_id
+        //$user = UserModel::firstWhere('level_id', 1);
+
+        // Ambil data dari tabel m_user berdasarkan kolom level_id
+        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+
+        // Ambil data dari tabel m_user berdasarkan kolom level_id
+        //$user = UserModel::findOrFail(1);
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view("user", ["data" => $user]);
     }
 }
